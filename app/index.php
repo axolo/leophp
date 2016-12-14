225577089b0b89eae5ff66b99c59946726f3cdd9
Yueming Fang <zergdo@gmail.com>
2016-12-14 10:58:09 +0800

    using composer

--- app/index.php
+++ app/index.php
@@ -2,7 +2,13 @@
 namespace leophp;
 defined('LEOPHP') || define('LEOPHP', '..' . DIRECTORY_SEPARATOR .'framework' . DIRECTORY_SEPARATOR);
 
-require_once(LEOPHP . 'App.php');
+// using composer require
+require '../vendor/autoload.php';
+use leophp\App as App;
+
+// using classic require
+// require_once(LEOPHP . 'App.php');
+
 $config = __DIR__ . DIRECTORY_SEPARATOR .'config' . DIRECTORY_SEPARATOR . 'config.php';
 
 App::run($config);
